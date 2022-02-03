9f150d55398bb482589cb0739f50f78dc1910fb9
Codatrix <ankur.codatrix@gmail.com>
2022-02-03 09:40:07 +0530

    Testing routes created for bootcamp - Initial testing of Routes started using POSTMAN by delegating routes to the bootcampRouter module

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-// import and Load env Vars
+// Import and Load env Vars
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
 // Express
 const express = require("express");
 const app = express();
+
+// Route files
+const bootcampRouter = require("./routes/bootcampRoutes");
+
+//--------------------------
+// Our ROUTES w/ Routers
+//--------------------------
+
+// Middlewares: Mounting the Routers Properly ✔✔
+app.use("/api/v1/bootcamps", bootcampRouter);
 
 // Server
 const PORT = process.env.PORT || 5000;
